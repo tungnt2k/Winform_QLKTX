e97392eb79845af7052102418aef470cd1b55c98
tung.nguyen <tung.nguyen@amela.vn>
2021-04-09 08:23:43 +0700

    Change any

--- QLKTX/Program.cs
+++ QLKTX/Program.cs
@@ -18,7 +18,7 @@ namespace QLKTX
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main());
+            Application.Run(new frm_DangNhap());
         }
     }
 }
